f8170c3e748e4c0aa0a39eb58cfcbd635097cdc7
Zane Lisbey <zlisbey@gmail.com>
2021-09-06 14:00:18 -0600

    Started adding functions to calculate tip amount per person

--- application.js
+++ application.js
@@ -1,3 +1,19 @@
-function displayTipPerPerson(valueTip){
-    document.getElementById("tipPerPerson").value+=valueTip;
+let totalExcludingTax = 0;
+let tipPercentage = 0;
+
+function tipPercent(percentAmount)
+{
+    tipPercentage = percentAmount;
+}
+
+function calculateTotalExcludingTax()
+{
+    totalExcludingTax = ($("#billAmount").val() / $("#numberOfPersons").val());
+}
+
+function tipPerPersonCalculation()
+{
+    let tipPerPerson = (totalExcludingTax * tipPercentage);
+    alert(tipPerPerson);
+    //$("#tipPerPerson").text(tipPerPerson);
 }
